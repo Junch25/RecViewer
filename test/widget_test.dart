e624b120f1a0c5398452c7032b2f068e7350ce0e
Junch25 <junch1225.public@gmail.com>
2020-06-14 17:34:15 +0900

    update test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,37 +1,24 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  testWidgets('finds a Text widget', (WidgetTester tester) async {
-    // Build an App with a Text widget that displays the letter 'H'.
+  testWidgets('webview widget', (WidgetTester tester) async {
+    final Completer<WebViewController> webView = Completer<WebViewController>();
     await tester.pumpWidget(MaterialApp(
       home: Scaffold(
-        body: Text('H'),
+        body: WebView(
+          initialUrl: 'https://flutter.dev/',
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController controller) {
+            webView.complete(controller);
+          },
+        ),
       ),
     ));
-
-    // Find a widget that displays the letter 'H'.
-    expect(find.text('H'), findsOneWidget);
-  });
-
-  testWidgets('finds a widget using a Key', (WidgetTester tester) async {
-    // Define the test key.
-    final testKey = Key('K');
-
-    // Build a MaterialApp with the testKey.
-    await tester.pumpWidget(MaterialApp(key: testKey, home: Container()));
-
-    // Find the MaterialApp widget using the testKey.
-    expect(find.byKey(testKey), findsOneWidget);
-  });
-
-  testWidgets('finds a specific instance', (WidgetTester tester) async {
-    final childWidget = Padding(padding: EdgeInsets.zero);
-
-    // Provide the childWidget to the Container.
-    await tester.pumpWidget(Container(child: childWidget));
-
-    // Search for the childWidget in the tree and verify it exists.
-    expect(find.byWidget(childWidget), findsOneWidget);
+    final WebViewController controller = await webView.future;
+    final String currentUrl = await controller.currentUrl();
+    expect(currentUrl, 'https://flutter.dev/');
   });
 }
